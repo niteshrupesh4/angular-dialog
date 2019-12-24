26d02ec65b08d5bb14d70d0070b86f5bbad27a6c
niteshrupesh4 <niteshrupesh4@gmail.com>
2019-12-24 12:53:33 +0530

    4 files modified Stripe charge

--- src/app/parent-child/parent/parent.component.ts
+++ src/app/parent-child/parent/parent.component.ts
@@ -12,6 +12,8 @@ componentRef: any;
   @ViewChild('loadComponent', { read: ViewContainerRef, static: false }) entry: ViewContainerRef;
   isAction: boolean = true;
   constructor(private resolver: ComponentFactoryResolver) { }
+
+ 
   
    data = [
     {
@@ -45,11 +47,61 @@ componentRef: any;
     this.componentRef.destroy();
   }
 
-  ngOnInit() {
-  }
+  
 
   onClick(){
     this.isAction = false;
+  }
+
+  handler:any = null;
+  ngOnInit() {
+  
+    this.loadStripe();
+  }
+  
+  pay(amount) {    
+  
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
+  
+    handler.open({
+      name: 'Angular 8',
+      description: 'Demo stripe pay',
+      amount: amount * 100
+    });
+  
+  }
+  
+  loadStripe() {
+      
+    if(!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      s.onload = () => {
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+          locale: 'auto',
+          token: function (token: any) {
+            // You can access the token ID with `token.id`.
+            // Get the token ID to your server-side code for use.
+            console.log(token)
+            alert('Payment Success!!');
+          }
+        });
+      }
+        
+      window.document.body.appendChild(s);
+    }
   }
 
 }
