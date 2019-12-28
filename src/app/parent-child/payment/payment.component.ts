53a4a5cc0fd2d4eabc9e8e1632154caa0eeef8a3
niteshrupesh4 <niteshrupesh4@gmail.com>
2019-12-28 21:54:44 +0530

    3 files modified

--- src/app/parent-child/payment/payment.component.ts
+++ src/app/parent-child/payment/payment.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-declare var paypal;
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -7,42 +6,8 @@ declare var paypal;
 })
 export class PaymentComponent implements OnInit {
 
-   @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
-
-  product = {
-    price: 777.77,
-    description: 'used couch, decent condition',
-    img: 'assets/couch.jpg'
-  };
-
-  paidFor = false;
-
   ngOnInit() {
-    paypal
-      .Buttons({
-        createOrder: (data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                description: this.product.description,
-                amount: {
-                  currency_code: 'USD',
-                  value: this.product.price
-                }
-              }
-            ]
-          });
-        },
-        onApprove: async (data, actions) => {
-          const order = await actions.order.capture();
-          this.paidFor = true;
-          console.log(order);
-        },
-        onError: err => {
-          console.log(err);
-        }
-      })
-      .render(this.paypalElement.nativeElement);
+   
   }
 
 }
