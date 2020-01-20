078c6fe6987f1c471412311773b2c8b60a4ce2e2
niteshrupesh4 <niteshrupesh4@gmail.com>
2020-01-20 22:10:13 +0530

    1 file modified

--- src/app/parent-child/child1/child1.component.ts
+++ src/app/parent-child/child1/child1.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
 @Component({
-  selector: 'app-child1',
-  templateUrl: './child1.component.html',
-  styleUrls: ['./child1.component.css']
+  selector: "app-child1",
+  templateUrl: "./child1.component.html",
+  styleUrls: ["./child1.component.css"]
 })
 export class Child1Component implements OnInit {
+  constructor(private location: Location) {}
+  count = 0;
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onClick() {
+    this.location.replaceState("/some/newstate/" + this.count++);
   }
-
 }
